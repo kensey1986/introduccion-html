251461da7fb5e3cce1115b7a08013db8cd5d77ba
Yivan2021 <manuel.rodriguez@softwareestrategico.com>
2022-03-30 10:05:21 -0500

    intro-css

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+function cambiarFondo(){
+    // console.log('====================================');
+    // console.log('dio clic');
+    // console.log('====================================');
+
+
+     const nodo = document.getElementById("parrafo");
+     console.log('===============nodo que quiero examinar=====================');
+     console.log(nodo);
+     console.log('====================================');
+     nodo.setAttribute('class', 'otroFondo');
+}
